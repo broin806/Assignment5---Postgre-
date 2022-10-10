d035b0413932b86bfd77f415c74d82ed799e300b
Broinson <broin890@gmail.com>
2022-10-09 22:48:42 -0800

    initial commit

---
+++ data-service.js
@@ -0,0 +1,86 @@
+//Declared globally and file reading operation
+var fs = require("fs");
+var departments = []; 
+var employees = []; 
+var managers = [];
+
+
+//exported initialize() function
+exports.initialize = () => {//ES6 snytax
+    return new Promise((resolve, reject) => { //returns a promise that passes the respective data with 'resolve method'  
+      fs.readFile("./data/employees.json", (err, data) => { //reads employees.json within "data" directory on server
+        if(err){
+          reject("Failure to read file employees.json!");
+        }
+      else{
+        employees = JSON.parse(data); //parsed object 
+        fs.readFile("./data/departments.json", (err, data) => { //reads department.json within "data" directory on server
+          if(err){
+            reject("unable to read file!");
+          }
+        else{
+          departments = JSON.parse(data); //parsed object 
+          resolve("FILES WAS A SUCCESS"); //only if both operations were successful, invoke resolve method to have promise communicate back to server.js
+        
+        }
+      });
+    }
+  });
+});
+} 
+
+//exported getAllEmployees() function
+exports.getAllEmployees = () => {
+
+  return new Promise(function(resolve,reject){
+    if(employees.length() != 0){  //returns full array of employee objects and uses resolve method
+    resolve(employees);  //resolve method 
+    }
+    else{
+    reject("No results has been returned.")
+    }
+})
+}
+
+
+  //exported getManagers() function
+  exports.getManagers = () => {
+
+    return new Promise(function(resolve, reject){
+
+    if (employees.length() != 0)  //employee array does not equal to 0 
+        { 
+            for(let e = 0; e < employees[e].isManager.length(); i++){ //loop through the employee's dataset 
+            if (employees.isManager){ //checking to see "employee" object exists in Manager dataset 
+            managers.push(employees[e]) //returns the new length of array in the employee set 
+            }
+          //end of resolve method
+            }
+        
+    }
+    else{
+        reject("No results returned")
+    }
+    resolve(managers); //resolve method
+    })
+
+  
+  }
+
+//exported getDepartments() function
+exports.getDepartments = () => {
+
+  return new Promise(function(resolve,reject){
+    if(departments.length() != 0){ //returns full array of department objects and uses resolve method
+    resolve(departments); //resolve method 
+    }
+    else{
+    reject("No results has been returned.")
+    }
+})
+}
+
+
+
+
+    
--- server.js
+++ server.js
@@ -1,1 +1,112 @@
-ghjghjgjyg
+/************************************************************************* * 
+ * BTI325– Assignment 2 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+ * No part * of this assignment has been copied manually or electronically from any other source * 
+ * (including 3rd party web sites) or distributed to other students. * * Name: Broinson Jeyarajah Student ID: 
+101501229 Date:  * 2020-10-09 * Your app’s URL (from Cyclic) : 
+*************************************************************************/
+
+//required module/path to access the data in the server.js file 
+var DATA_SERVICE = require("data-service.js")
+
+//express.js module
+var express = require("express");
+var app = express(); //express is a function
+
+var path = require("path"); // include moduel path to use __dirname, and function path.join()
+
+//Server listens on this PORT 
+var HTTP_PORT = process.env.PORT || 8080;  
+
+// function which triggers the http server to listen for requests 
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+//"static" middleware for css/site.css 
+app.use(express.static('public'));
+
+
+//HOME AND ABOUT ROUTE 
+
+ // directs to the home route
+app.get("/", function (req, res){ //function is represented as the home page
+    res.sendFile(path.join(__dirname,"/views/home.html")); //takes the address to home.html
+});
+
+//route that returns the about page 
+app.get("/about", function(req,res){ //function is represented as the about page 
+    res.sendFile(path.join(__dirname,"/views/about.html")); //takes the address to about.html
+  }); 
+
+
+
+  //ADDITIONAL ROUTES 
+
+//responds to employees page's get requests
+app.get("/employees", function(req,res){ //function is represented as the employees page
+  res.sendFile(path.join(__dirname,"./data/employees.json"));
+  DATA_SERVICE.getAllEmployees().then(function(data){ //Makes the call to the respective get() method to fetch data
+    res.json(data);//access the data from the function and send response back to client. 
+})
+.catch(function(err){//if any methods are intended to become unsuccessful
+  res.json({"message" : err});
+})
+
+})
+
+
+
+
+  //responds to departments page's get requests
+app.get("/departments", function(req,res){ //function is represented as the departments page
+    res.sendFile(path.join(__dirname,"./data/departments.json"));
+    DATA_SERVICE.getDepartments().then(function(data){ //Makes the call to the respective get() method to fetch data
+      res.json(data);//access the data from the function and send response back to client. 
+  })
+  .catch(function(err){ //if any methods are intended to become unsuccessful
+    res.json({"message" : err});
+  })
+
+})
+
+
+ //responds to managers page's get requests
+app.get("/managers", function(req,res){ //function is represented as the managers page
+    res.sendFile(path.join(__dirname,"./data/managers.json"));
+    DATA_SERVICE.getManagers().then(function(data){ //Makes the call to the respective get() method to fetch data
+      res.json(data); //access the data from the function and send response back to client. 
+  })
+  .catch(function(err){ //if any methods are intended to become unsuccessful
+    res.json({"message" : err});
+  })
+
+})
+
+
+
+//setup http server to listen on HTTP_PORT
+
+DATA_SERVICE.initialize().then(function(data){ //call app.listen() and the initialize function is successful. 
+app.listen(HTTP_PORT, onHttpStart);
+
+let err = "There is no data to fetch!"; 
+
+}).catch(function(err){// display the catch function as initialize method invoked reject method
+  console.log(err);
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
