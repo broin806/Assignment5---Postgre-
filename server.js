f87a1fccc4a2165f27833ea26bf521776758c851
Broinson <broin890@gmail.com>
2022-11-14 12:09:47 -0800

    git initial

--- server.js
+++ server.js
@@ -16,8 +16,46 @@ var app = express(); //express is a function
 var path = require('path'); 
 var fs = require('fs');
 var HTTP_PORT = process.env.PORT || 8080;  
-var multer = require('multer'); //required module 
+var multer = require('multer'); //required module
+const bodyParser = require("body-parser"); 
+var exphbs = require('express-handlebars');
 
+
+app.engine('.hbs',exphbs.engine({ extname:'.hbs', defaultLayout:'main', 
+//Handlebars custom "helper"
+helpers:{ navLink:function(url, options){
+  return '<li' + ((url==app.locals.activeRoute)? ' class="active"':'')
+  +'><a href="'+url+'">'+options.fn(this)+'</a></li>'
+},
+//Handlebars custom "helper"
+equal:function(lvalue, rvalue, options){
+    if(arguments.length<3)
+        throw new Error("Handlerbars Helper equal needs 2 parameters");
+    if(lvalue != rvalue){
+        return options.inverse(this);
+    }else{
+        return options.fn(this);
+    }
+}
+}
+}));
+
+
+
+app.set('view engine', '.hbs'); 
+
+
+// const HTTP_PORT = process.env.PORT || 8080;
+
+//"static" middleware for css/site.css 
+app.use(express.static('public'));
+
+
+
+// function which triggers the http server to listen for requests 
+function onHttpStart(){
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
 
 
 //defining a storage variable using "multer.diskStorage"
@@ -28,40 +66,138 @@ var storage = multer.diskStorage({
   }
 }); 
 
-//"static" middleware for css/site.css 
-app.use(express.static('public'));
-//body-parser
-//handle form data without file upload.
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// //body-parser
+// //handle form data without file upload.
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+
 //Define an "upload" variable as multer({ storage: storage });
 const upload = multer({ storage: storage });
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//Fixing the Navigation Bar to Show the correct "active" item
+app.use(function(req,res,next){
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+  next();
+  });
+
+
 
 
 //HOME AND ABOUT ROUTE 
 
  // directs to the home route
 app.get("/", (req, res)=>{ 
-    res.sendFile(path.join(__dirname,"/views/home.html")); //takes the address to home.html
+    res.render("home");
+   // res.sendFile(path.join(__dirname,"/views/home.html")); //takes the address to home.html
 });
+
+
+// app.get("/home", (req, res)=>{ 
+//   res.render(path.join(__dirname,"/views/home.hbs")); //takes the address to home.hbs
+// });
 
 //route that returns the about page 
 app.get("/about", (req,res)=>{ 
-    res.sendFile(path.join(__dirname,"/views/about.html")); //takes the address to about.html
+    res.render("about");
+    //res.sendFile(path.join(__dirname,"/views/about.html")); //takes the address to about.html
   }); 
 
 
 
   //-----------ADDITIONAL ROUTES 
+//Part 4: Adding New Routes to query "Employees"
+//responds to employees page's get requests
+app.get("/employees", (req,res)=>{ 
 
+  if(req.query.status){ //if query string is for status info
+  ///employees?status=value
+  data_service.getEmployeesByStatus(req.query.status).then((data)=>{ 
+    res.json(data);//access the data from the function and send response back to client. *JSON String*
+      }).catch((err)=>{ 
+      res.json({message : err});
+        //console.log(err);
+    });
+  }
+  
+  else if (req.query.department){ //if query string is for department info
+    ////employees?department=value
+    data_service.getEmployeesByDepartment(req.query.department).then((data)=>{ 
+      res.json(data);//access the data from the function and send response back to client. *JSON String*
+        }).catch((err)=>{ 
+        res.json({message : err});
+          //console.log(err);
+      })
+    }
+    
+  
+    else if (req.query.manager){ //if query string is for managers info
+      ////employees?manager=value
+      data_service.getEmployeesByManager(req.query.manager).then((data)=>{ 
+        res.json(data);//access the data from the function and send response back to client. *JSON String*
+          }).catch((err)=>{ 
+          res.json({message : err});
+            //console.log(err);
+        })
+  
+    }else{ //REQUEST DOESNT CONTAIN STRING and return(s) all employees without filters using *JSON String*
+        data_service.getAllEmployees().then((data)=>{ 
+          res.json(data);//access the data from the function and send response back to client. 
+            }).catch((err)=>{ 
+            res.json({message : err});
+              //console.log(err);
+     });
+    }
+  })
+
+
+  app.get("/employee/:empNum",(req,res)=>{
+    data_service.getEmployeeByNum(req.params.empNum).then((data)=>{
+        res.json(data);
+    }).catch((err)=>res.json({message: err}));
+})
+
+
+//  //responds to managers page's get requests
+//  app.get("/managers", (req,res)=>{ 
+//   data_service.getManagers().then((data)=>{ //Makes the call to the respective get() method to fetch data
+//       res.json(data); //access the data from the function and send response back to client. 
+//   })
+//   .catch((err)=>{ 
+//     res.json({"message" : err});
+//       //console.log(err);
+
+//   })
+
+// }) 
+
+  //responds to departments page's get requests
+  app.get("/departments", (req,res) => { 
+    data_service.getDepartments().then((data)=>{ //Makes the call to the respective get() method to fetch data
+        res.json(data);//access the data from the function and send response back to client. 
+      }).catch((err)=>{ 
+      res.json({"message" : err});
+        //console.log(err);
+  
+    })
+  
+  })
+  
+
+  
 //responds to addEmployees get requests
-app.get("/employees/add", (req,res)=>{   
-  res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+app.get("/employees/add", (req,res)=>{ 
+  res.render("addEmployee");  
+  //res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
 });
 
 //responds to addImages get requests
 app.get("/images/add", (req,res)=>{  
-  res.sendFile(path.join(__dirname,"/views/addImage.html"));
+  res.render("addImage");
+  // res.sendFile(path.join(__dirname,"/views/addImage.html"));
 }); 
 
 
@@ -84,61 +220,13 @@ app.get("/images", (req,res) => {
 
 //route makes a call to the (promise-driven) function from the data-service.js module
 app.post("/employees/add", function(req,res){
-  //redirects to /employees
-  data_service.addEmployee(req.body).then(res.redirect('/employees'))
-  
  //redirects to /employees
-   //resolve(data); //resolves successfully 
-   .catch((err)=>{   
-      res.json({"message" : err});
-      //console.log(err);
-  })
-}) 
+ data_service.addEmployee(req.body)
+ res.redirect('/employees');
+});
 
 
 
-
-//Part 4: Adding New Routes to query "Employees"
-//responds to employees page's get requests
-app.get("/employees", (req,res)=>{ 
-
-if(req.query.status){ //if query string is for status info
-///employees?status=value
-data_service.getEmployeesByStatus(req.query.status).then((data)=>{ 
-  res.json(data);//access the data from the function and send response back to client. *JSON String*
-    }).catch((err)=>{ 
-    res.json({"message" : err});
-      //console.log(err);
-  })
-}
-
-else if (req.query.department){ //if query string is for department info
-  ////employees?department=value
-  data_service.getEmployeesByDepartment(req.query.department).then((data)=>{ 
-    res.json(data);//access the data from the function and send response back to client. *JSON String*
-      }).catch((err)=>{ 
-      res.json({"message" : err});
-        //console.log(err);
-    })
-  }
-
-  else if (req.query.manager){ //if query string is for managers info
-    ////employees?manager=value
-    data_service.getEmployeesByManager(req.query.manager).then((data)=>{ 
-      res.json(data);//access the data from the function and send response back to client. *JSON String*
-        }).catch((err)=>{ 
-        res.json({"message" : err});
-          //console.log(err);
-      })
-
-  }else{ //REQUEST DOESNT CONTAIN STRING and return(s) all employees without filters using *JSON String*
-      data_service.getAllEmployees().then((data)=>{ 
-        res.json(data);//access the data from the function and send response back to client. 
-          }).catch((err)=>{ 
-          res.json({"message" : err});
-            //console.log(err);
-   });
-  
 
 
   // data_service.getAllEmployees().then((data)=>{ //Makes the call to the respective get() method to fetch data
@@ -149,32 +237,6 @@ else if (req.query.department){ //if query string is for department info
   // //console.log(err);
 
 
-
-
-  //responds to departments page's get requests
-app.get("/departments", (req,res) => { 
-  data_service.getDepartments().then((data)=>{ //Makes the call to the respective get() method to fetch data
-      res.json(data);//access the data from the function and send response back to client. 
-    }).catch((err)=>{ 
-    res.json({"message" : err});
-      //console.log(err);
-
-  })
-
-})
-
- //responds to managers page's get requests
-app.get("/managers", (req,res)=>{ 
-  data_service.getManagers().then((data)=>{ //Makes the call to the respective get() method to fetch data
-      res.json(data); //access the data from the function and send response back to client. 
-  })
-  .catch((err)=>{ 
-    res.json({"message" : err});
-      //console.log(err);
-
-  })
-
-}) 
 
 
 
@@ -189,17 +251,8 @@ app.use((req,res)=>{ //404 error message
 data_service.initialize().then(()=>{ //call app.listen() and the initialize function is successful. 
 app.listen(HTTP_PORT, onHttpStart); //listen on HTTP_PORT 
 
-// function which triggers the http server to listen for requests 
-function onHttpStart(){
-  console.log("Express http server listening on: " + HTTP_PORT);
-}
-
-
-
 }).catch(()=>{// display the catch function as initialize method invoked reject method
   console.log("Cannot open files!");
-})
+}) 
 
-  }
-}); 
 
